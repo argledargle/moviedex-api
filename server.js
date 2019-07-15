80f90eeeca3ff9e4e6d168e459287cd1f581f727
Alex Peter <apyells@gmail.com>
2019-07-15 08:59:26 -0500

    updates .env file and server for API_TOKEN

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(rq, res, next) {
-  const apiToken = process.env.API_TOKEN; //I NEED TO CREATE AN ENV FILE FOR THE TOKEN;
-  const authToken = console.log("Validate bearer token middleware"); //I need to create the auth token and have it in req.headers.authorization
+  const apiToken = process.env.API_TOKEN; 
+  const authToken = console.log("Validate bearer token middleware");
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
@@ -45,8 +45,6 @@ app.get("/movie", function handleGetMovie(req, res) {
   res.json(response)
 });
 
-const PORT = 9000
-
-app.listen(PORT, ()=> {
-    console.log(`Sever listening at http://localhost:${PORT}`)
+app.listen(9000, ()=> {
+    console.log(`Sever listening at http://localhost:9000`)
 })
